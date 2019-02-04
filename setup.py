c57be99e6dc6cdd163039e10497a0e24779c34e1
Felix <fyz11235@hotmail.com>
2019-02-04 13:03:22 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='centriole_distancing',
+      version='0.1',
+      description='A simple library for centriole distancing',
+      url='http://github.com/fyz11/Centriole_Distancing',
+      author='Felix Y. Zhou',
+      author_email='felixzhou1@gmail.com',
+      license='MIT',
+      packages=['centriole_distancing'],
+      zip_safe=False)
